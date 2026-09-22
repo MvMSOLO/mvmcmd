e45f7979ec20ec62dcdf965b2561719df634cc2c
MvMSOLO <mirzayevavazbek569@gmail.com>
2026-09-22 22:05:40 +0500

    fix(android): register native launcher plugin

--- android/app/src/main/java/com/mvmcmd/launcher/MainActivity.java
+++ android/app/src/main/java/com/mvmcmd/launcher/MainActivity.java
@@ -2,4 +2,8 @@ package com.mvmcmd.launcher;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    public MainActivity() {
+        registerPlugin(MvmLauncherPlugin.class);
+    }
+}
